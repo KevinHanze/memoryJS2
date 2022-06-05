ae82bf1e8be726f1030f0f8e68ebaf06db818f37
KevinHanze <58030467+KevinHanze@users.noreply.github.com>
2022-06-05 21:53:33 +0200

    pictures on cards change

--- memory.js
+++ memory.js
@@ -1,8 +1,8 @@
 const cards = document.querySelectorAll('.cell')
 const victoryImg = document.getElementById("victory-img")
 const newGameButton = document.getElementById("new-game-button")
-const loadedImages = [];
 const imagesUrl = "https://picsum.photos/300";
+const dogImagesUrl = "https://dog.ceo/api/breeds/image/random"
 
 const options = {
     method: "GET"
@@ -12,12 +12,13 @@ let cardFlipped = false;
 let firstCard, secondCard
 let boardLocked = false;
 let cardsFlipped = 0;
+let loadedImages = [];
 
 cards.forEach(card => card.addEventListener("click", flipCard))
 victoryImg.addEventListener("click", resetBoard)
-newGameButton.addEventListener("click", loadImages)
+newGameButton.addEventListener("click", kaas)
 
-function flipCard(event) {
+function flipCard() {
     if (boardLocked) {return;}
     if (this === firstCard) {return;}
     this.classList.toggle('flip')
@@ -76,66 +77,114 @@ function resetBoard() {
     secondCard = null;
 
     window.location.reload();
+
+    document.getElementById("card-dropdown").selectedIndex = localStorage.getItem("picture-choice")
+
 }
 
-function asignLetters() {
+function kaas() {
+    console.log(sessionStorage.getItem("picture-choice")) ;
+}
+
+function shuffleCards() {
     cards.forEach(card => {
         card.style.order = Math.floor(Math.random() * 36);
      })
 }
 
-function fetchImage() {
-    const imgUrl = "https://picsum.photos/300";
-    (async () => {
-        const response = await fetch(imgUrl);
-        const imageBlob = await response.blob();
-        const reader = new FileReader();
-        reader.readAsDataURL(imageBlob);
-        reader.onloadend = () => {
-            const base64data = reader.result;
-        }
-    })()
+function loadStartingImages() {
+    if (sessionStorage.getItem("picture-choice") === 'random') {
+        loadPicsumImages()
+    } else {
+        loadDogImages()
+    }
 }
 
-async function loadImages() {
-
+async function loadDogImages() {
+    boardLocked = true;
     for (let i = 0; i < 18; i++) {
-    let response = await fetch(imagesUrl, options)
-
-    if (response.status === 200) {
-        const imgBlob = await response.blob()
-        const imageObjectURL = URL.createObjectURL(imgBlob)
-
-        const image = document.createElement("img")
-        image.src = imageObjectURL;
-
-        loadedImages.push(imageObjectURL)
-
+        await fetch(dogImagesUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                loadedImages.push(data.message)
+            })
     }
-    else {
-        console.log("HTTP-Error" + response.status)
+    asignImage();
+    boardLocked = false;
+}
+
+    async function loadPicsumImages() {
+        boardLocked = true;
+        for (let i = 0; i < 18; i++) {
+            let response = await fetch(imagesUrl, options)
+
+            if (response.status === 200) {
+                const imgBlob = await response.blob()
+                const imageObjectURL = URL.createObjectURL(imgBlob)
+
+                const image = document.createElement("img")
+                image.src = imageObjectURL;
+
+                loadedImages.push(imageObjectURL)
+
+            } else {
+                console.log("HTTP-Error" + response.status)
+            }
+        }
+
+        asignImage();
+        boardLocked = false;
+    }
+
+    function asignImage() {
+        for (let i = 0; i < 18; i++) {
+            let j = i + 18;
+            let img = document.createElement("img");
+            let img2 = document.createElement("img");
+            img.src = loadedImages[i];
+            img2.src = loadedImages[i];
+            cards[i].appendChild(img);
+            cards[j].appendChild(img2);
+        }
+
+        shuffleCards();
+    }
+
+    function removeImages() {
+    for (let i =0; i < cards.length; i++) {
+        if (cards[i].firstChild) {
+            cards[i].removeChild(cards[i].firstChild)
+            }
         }
     }
 
-    asignImage();
-}
-
-function asignImage() {
-    for (let i =0; i < 18; i++) {
-        let j = i+18;
-        let img = document.createElement("img");
-        let img2 = document.createElement("img");
-        img.src = loadedImages[i];
-        img2.src = loadedImages[i];
-        document.getElementsByClassName('cell')[i].appendChild(img);
-        document.getElementsByClassName('cell')[j].appendChild(img2);
+    function changeColor() {
+        var defaultColor = document.getElementById("card-color").value;
+        document.documentElement.style.setProperty('--colorclosed', defaultColor);
     }
+    function changeCardPictures() {
+        let menu = document.getElementById("card-dropdown");
+        let menuValue = menu.value;
+
+        sessionStorage.clear();
+        sessionStorage.setItem("picture-choice", menuValue)
+
+        if (sessionStorage.getItem("picture-choice") === "dogs") {
+            loadedImages = [];
+            removeImages()
+            loadDogImages();
+        } else if (sessionStorage.getItem("picture-choice") === "random") {
+            loadedImages = [];
+            removeImages()
+            loadPicsumImages();
+        }
+        else {
+            loadedImages = [];
+            removeImages()
+            loadDogImages();
+        }
 }
 
-function changeColor() {
-    var defaultColor = document.getElementById("card-color").value;
-    document.documentElement.style.setProperty('--colorclosed', defaultColor)
-}
 
 
 
